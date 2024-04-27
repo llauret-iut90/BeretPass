6ddcc8a53601f92aa027a1c4818d378b5555f921
lauret lucas <lucas.lauret@edu.univ-fcomte.fr>
2024-04-28 01:06:34 +0200

    Refonte ui login plus ajout image dans resources

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -1,11 +1,14 @@
 #include "headers/mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QPixmap>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    QPixmap pix(":/img/resources/BeretPassLogo.png");
+    ui ->label_pic->setPixmap(pix.scaled(200, 200));
 }
 
 MainWindow::~MainWindow()
