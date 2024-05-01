60968cc20dfec234191297cecdf23735ea7e2be6
lauret lucas <lucas.lauret@edu.univ-fcomte.fr>
2024-05-01 21:43:11 +0200

    ajout de QStackedWidget pour gérer plusieurs pages vérification de mot de passe (faudra mettre une bdd) et interface très médiocre du password manager

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -1,6 +1,7 @@
 #include "headers/mainwindow.h"
 #include "ui_mainwindow.h"
 #include <QPixmap>
+#include <QMessageBox>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
@@ -15,3 +16,25 @@ MainWindow::~MainWindow()
 {
     delete ui;
 }
+
+void MainWindow::on_Login_clicked()
+{
+    QString username = ui->inputUsername->text();
+    QString password = ui->inputPassword->text();
+    QString message = QString("Salut à toi %1").arg(username);
+
+    if(username == "admin" && password == "admin"){
+        QMessageBox::information(this, "Login", message);
+        ui->stackedWidget->setCurrentIndex(1);
+
+    }else {
+        QMessageBox::warning(this, "Login", "Tu n'es pas la personne que tu pretends être !");
+    }
+}
+
+
+void MainWindow::on_Register_clicked()
+{
+
+}
+
